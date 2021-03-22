7411974845c5cf8674e4c062e45416135e2ede13
GabrielaParedes27 <78928248+GabrielaParedes27@users.noreply.github.com>
2021-03-22 00:05:02 -0600

    Added current forecast

--- src/app.js
+++ src/app.js
@@ -95,12 +95,13 @@ function handleSubmit(event) {
     search(cityInputElement.value);
 }
 
-
 function searchLocation(position) {
     let apiKey = "816a63a33af440332c05784e3d9896ea";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemp);
     
+    apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentLocation(event) {
